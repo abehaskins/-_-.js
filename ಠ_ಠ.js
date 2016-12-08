2c47ffa0c7944e8b0d35a41eea07a9dafb49b3e6
zamadatix <zamadatix@gmail.com>
2016-12-08 14:16:31 -0500

    Compatibility with Node.js
    Per the discussion on Reddit from /u/daxxcqb https://www.reddit.com/r/javascript/comments/5h6ng1/%E0%B2%A0_%E0%B2%A0js/daxxcqb/ change "window.console.warn" to "self.console.warn" for compatibility with node.

--- ಠ_ಠ.js
+++ ಠ_ಠ.js
@@ -1,10 +1,10 @@
 (function (self) {
   "use strict";
   if ((typeof console !== 'undefined') && (typeof console.warn === 'function')) {
-    var warn = window.console.warn;
+    var warn = self.console.warn;
     self['ಠ_ಠ'] = Function.prototype.bind.call(warn, console);
-    window.console.warn = undefined;
+    self.console.warn = undefined;
   } else {
     self['ಠ_ಠ'] = function () {}
   }
-}(typeof window !== 'undefined'? window : typeof global !== 'undefined' ? global : self));
+}(typeof self !== 'undefined'? self : typeof global !== 'undefined' ? global : self));
